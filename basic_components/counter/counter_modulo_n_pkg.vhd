bdc3905b6e2abd86a363b31c9d9d2cf3654f724d
Antonio Riccio <antonio.riccio.27@gmail.com>
2020-07-30 14:42:36 +0200

    counter: update pkg

--- basic_components/counter/counter_modulo_n_pkg.vhd
+++ basic_components/counter/counter_modulo_n_pkg.vhd
@@ -1,3 +1,11 @@
+----------------------------------------------------------------------------------
+--! @file counter_modulo_n_pkg.vhd
+--! @author Antonio Riccio
+--! @brief Configuration values for the counter_modulo_n
+--! @version 1.0
+--! @date 30/07/2020
+--! @copyright GNU Public License
+----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 
